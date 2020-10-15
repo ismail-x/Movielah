69d26991d2a37622605e8c060f84a579a2dac873
ismail <ismail.x@aol.com>
2020-10-15 22:11:53 +0700

    Initial Commit

---
+++ NBSMovie/NBSMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBSMovieApp.swift
+//  NBSMovie
+//
+//  Created by Ismail . on 15/10/20.
+//
+
+import SwiftUI
+
+@main
+struct NBSMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
